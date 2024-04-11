b19d50002c036d53c003f01df725fa9f19e55c1b
‘zhandehuang’ <it_zdh@163.com>
2024-04-11 18:52:30 +0800

    Site updated: 2024-04-11 18:52:28

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["2024/03/14/hello-world/","2024/03/14/markdown使用笔记/","2024/04/10/test/","2024/04/09/常见算法/","2024/03/14/算法学习笔记/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
